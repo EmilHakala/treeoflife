dd22cdfbb6ad2b1ce099786ef2fd421394cd4b98
Emil Hakala <64849539+EmilHakala@users.noreply.github.com>
2025-07-24 00:38:47 +0300

    Started making enemy instantiation
    NEED TO FIX SIGNALS

--- Player/hud.gd
+++ Player/hud.gd
@@ -5,6 +5,13 @@ extends Control
 @onready var ammo: Label = $Ammo
 @onready var tree_health: ProgressBar = $TreeContainer/TreeHealth
 @onready var pointer: TextureRect = $Clock/pointer
+@onready var day_label: Label = $DayLabel
+
+
+#Other variables
+var is_fading : bool = false
+var fade_timer : float = 0.0
+var fade_duration :float = 3.0 # seconds
 
 
 #Stat Objects
@@ -14,15 +21,30 @@ extends Control
 
 
 func _process(delta: float) -> void:
+	
+	if is_fading:
+		fade_timer += delta
+		var t = 1.0 - clamp(fade_timer / fade_duration, 0.0, 1.0)
+		day_label.modulate.a = t
+		if t <= 0.0:
+			day_label.visible = false
+			is_fading = false
+			
 	var t = dn_system.time_of_day # 0.0 to 1.0
 	var angle = t * 360.0 # Full day = full rotation
 
 	var mat := pointer.material as ShaderMaterial
-	mat.set_shader_parameter("rotation_deg", angle)
+	mat.set_shader_parameter("rotation_deg", angle)	
 	
 func _ready() -> void:
 	update_hud()
+	new_day(1)
 
+func new_day(day):
+	day_label.visible = true
+	day_label.text = "Day: " + str(day)
+	fade_timer = 0.0
+	is_fading = true
 
 func update_hud():
 	player_health.value = player_character.health
--- Systems/DayNightSystem/dn_system.gd
+++ Systems/DayNightSystem/dn_system.gd
@@ -1,15 +1,51 @@
 extends Node3D
 
 
-@export var day_length : float = 120.0 # In seconds, full day cycle
+@onready var hud: Control = $"../HUD"
+
+@export var day_length : float = 60.0 # 6 minute 24h cycle
 var time_of_day : float = 0.0 # 0.0 to 1.0
+var day_counter : int = 1
+
+var enemies_to_spawn := 0
+var enemies_spawned := 0
+var spawn_interval := 0.0
+var spawn_timer := 0.0
+var next_spawn_delay := 0.0
+const enemy_scene = preload("res://enemies/simple_enemy.tscn")
+@onready var player_character: PlayerCharacter = $"../PlayerCharacter"
+@onready var tree: Node3D = $"../Tree"
+
 
 func _process(delta):
 	time_of_day += delta / day_length
 	if time_of_day > 1.0:
 		time_of_day = 0.0
-
-	
+		day_counter += 1
+		hud.new_day(day_counter)
+		setup_day_spawn(day_counter)
+		
+	# Only spawn during daytime (0.0 - 0.5)
+	if time_of_day <= 0.5 and enemies_spawned < enemies_to_spawn:
+		spawn_timer += delta
+		if spawn_timer >= next_spawn_delay:
+			spawn_enemy()
+			spawn_timer = 0.0
+			next_spawn_delay = randf_range(2.0, 5.0) # Adjust range to your liking
 	#sun.rotation_degrees.x = angle
 
 	#update_environment(time_of_day)
+	
+func setup_day_spawn(day: int):
+	enemies_to_spawn = 2 + int(day * 1.5)
+	enemies_spawned = 0
+	spawn_timer = 0.0
+	next_spawn_delay = randf_range(1.0, 3.0) # First delay of the day
+
+func spawn_enemy():
+	var spawn_point: Vector3 = global_transform.origin
+	var enemy = enemy_scene.instantiate()
+	enemy.collided_with_player.connect(player_character._on_simple_enemy_collided_with_player)
+	add_child(enemy)
+	enemy.global_transform.origin = spawn_point
+	enemies_spawned += 1
